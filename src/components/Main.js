dbefbc5ae397e67fc83d6d23c2c3a6a7a3883995
Teerasej <teerasej@gmail.com>
2021-03-18 15:11:11 +0700

    กดหนดตัวแปรที่แสดงถึงการ Login ถ้ามีการ Login ของ User เรียบร้อย (check จาก token) ให้แสดงหน้าแอพปกติ ถ้ายังไม่ Login ให้แสดงไปที่หน้า Login แทน

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,28 @@
 
 import React, { useState, useEffect} from 'react'
 import { CircularProgress } from "@material-ui/core";
+import { useHistory } from 'react-router';
 
 export default function Main() {
 
     const [onLoading, setOnLoading] = useState(true);
+    const history = useHistory();
+
+    let alreadyLogin = false;
+
 
     useEffect(() => {
         // check token 
 
+
         // หยุดแสดงตัวโหลด
         setOnLoading(false);
+
+        if(!alreadyLogin) {
+          history.push('/login');
+        }
+
+        
     }, []);
 
 

9374ff2ba353ceeefb24aff6ed78ff235d27700f
Teerasej <teerasej@gmail.com>
2021-03-18 15:00:07 +0700

    สร้าง state เพื่อแสดงตัวโหลดตอนที่แอพยังไม่พร้อม แอพจะพร้อมเมื่อเช็ค token เสร็จ เมื่อพร้อมแล้วก็จะแสดงเป็นหน้าสมาชิก

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,18 @@
 
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import { CircularProgress } from "@material-ui/core";
 
 export default function Main() {
 
-    return (
-        <div>Home</div>
-    );
+    const [onLoading, setOnLoading] = useState(true);
 
+    if (onLoading) {
+        return (
+            <CircularProgress />
+        );
+    } else {
+        return (
+            <div>Home</div>
+        );
+    }
 }

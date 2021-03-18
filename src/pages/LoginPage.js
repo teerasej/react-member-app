d09470cae536b3646904cbf13c6471203d15067c
Teerasej <teerasej@gmail.com>
2021-03-18 15:13:43 +0700

    สร้าง User Interface หน้า Login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,39 @@
 
 import React from 'react'
+import { Button, TextField } from '@material-ui/core'
 
 export default function LoginPage() {
     return (
+
         <div>
-            Login Page
+            <h1>Login: </h1>
+            <form>
+                <div>
+                    <TextField
+                        id="email"
+                        name="email"
+                        label="Email"
+                    />
+                </div>
+                <div>
+                    <TextField
+                        id="password"
+                        name="password"
+                        label="Password"
+                        type="password"
+                    />
+                </div>
+                <div>
+                    <Button variant="contained" type="submit">
+                        Sign in
+                    </Button>
+                </div>
+                <div>
+                    <Button>
+                        Create Account?
+                    </Button>
+                </div>
+            </form>
         </div>
     )
 }

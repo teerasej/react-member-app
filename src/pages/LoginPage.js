acaed86cf9c06310994beadfcf145b6cfe0d08be
Teerasej <teerasej@gmail.com>
2021-03-18 15:34:02 +0700

    นำ formik และ yup มาใช้กับหน้า Login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -4,18 +4,51 @@ import { Button, TextField } from '@material-ui/core'
 import {
     Link
   } from "react-router-dom";
+import * as yup from 'yup';
+import { useFormik } from 'formik';
 
 export default function LoginPage() {
+
+    const validationSchema = yup.object({
+        email: yup
+            .string()
+            .email('Enter a valid email')
+            .required('Email is required'),
+        password: yup
+            .string()
+            .min(4, 'Password should be of minimum 4 characters length')
+            .required('Password is required'),
+    });
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            
+            const jsonFormData = JSON.stringify(values);
+            console.log('Sign up Form:', jsonFormData);
+            
+        },
+    });
+
+
     return (
 
         <div>
             <h1>Login: </h1>
-            <form>
-                <div>
+            <form onSubmit={formik.handleSubmit}>
+            <div>
                     <TextField
                         id="email"
                         name="email"
                         label="Email"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        error={formik.touched.email && Boolean(formik.errors.email)}
+                        helperText={formik.touched.email && formik.errors.email}
                     />
                 </div>
                 <div>
@@ -24,6 +57,10 @@ export default function LoginPage() {
                         name="password"
                         label="Password"
                         type="password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        error={formik.touched.password && Boolean(formik.errors.password)}
+                        helperText={formik.touched.password && formik.errors.password}
                     />
                 </div>
                 <div>

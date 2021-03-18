7844d9592323b6c38f9902221f98b18d8d7e8808
Teerasej <teerasej@gmail.com>
2021-03-18 15:04:59 +0700

    สร้างโครง Navigation ด้วย Router - กำหนด Route / ใช้แสดง Main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div>
-      <Main></Main>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Main/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
